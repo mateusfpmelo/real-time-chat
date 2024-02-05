2de522c8b2d8321ae024111d773f071311ea317a
Mateus Melo <mateusfpmelodev@gmail.com>
2024-02-05 18:21:17 -0300

    checkpoint - react || inicio projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='projectchatrealtime'>
+      <div className='projectchatrealtime_title'><b>Olá,</b></div>
+      <label>Informe seu e-mail:</label>
+      <input type='text' className='projectchatrealtime_input' placeholder='Digite seu e-mail'></input>
+      <button>Continuar</button>
+    </div>
+  );
+}
+
+export default App;
