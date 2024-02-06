f3bcc58dd602389f73653d6bdbfbaaf50c157942
Mateus Melo <mateusfpmelodev@gmail.com>
2024-02-06 19:35:00 -0300

    button logout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -128,77 +128,84 @@ const Home = () => {
 
 
     }
+
+    const handleLogout = () => {
+        navigate('/logout')
+    }
     return(
         <div className='projectChatRealTimeHome'>
-            <div className='projectChatRealTimeHome_title'>
-                <div className='projectChatRealTimeHome_textYellow'>Olá, </div>
-                {showUserName &&<div>  {showUserName.toUpperCase()}!</div>}
-                
-            </div>
-            {contentChatRooms != '' &&
-                <>
-                    <br/>
-                    <label><b className='projectChatRealTimeHome_textYellow'>S A L A S</b></label>
-                    <br/>
-                </>
-            }
-            {contentChatRooms && contentChatRooms.map((room, index) => (
-                    <div key={index} className='projectChatRealTimeHome_rooms'>
-                        {room.accessType === 'public' || (room.accessType === 'private' && room.emailAccessList.includes(userLogged)) ? (
-                            <>
-                                <div className='projectChatRealTimeHome_roomsItem'>
-                                    {room.accessType === 'private' ? <MdOutlinePrivacyTip size={24} /> : <MdPublic size={24} />}
-                                    <div>{room.name.toUpperCase()} </div>
+            <>
+                <div className='projectChatRealTimeHome_title'>
+                    <div className='projectChatRealTimeHome_textYellow'>Olá, </div>
+                    {showUserName &&<div>  {showUserName.toUpperCase()}!</div>}
+                    
+                </div>
+                {contentChatRooms != '' &&
+                    <>
+                        <br/>
+                        <label><b className='projectChatRealTimeHome_textYellow'>S A L A S</b></label>
+                        <br/>
+                    </>
+                }
+                {contentChatRooms && contentChatRooms.map((room, index) => (
+                        <div key={index} className='projectChatRealTimeHome_rooms'>
+                            {room.accessType === 'public' || (room.accessType === 'private' && room.emailAccessList.includes(userLogged)) ? (
+                                <>
+                                    <div className='projectChatRealTimeHome_roomsItem'>
+                                        {room.accessType === 'private' ? <MdOutlinePrivacyTip size={24} /> : <MdPublic size={24} />}
+                                        <div>{room.name.toUpperCase()} </div>
+                                        
+                                    </div>
+                                    <div className='projectChatRealTimeHome_roomsDescription'>Descrição: {room.description}</div>
                                     
-                                </div>
-                                <div className='projectChatRealTimeHome_roomsDescription'>Descrição: {room.description}</div>
-                                
-                                <div className='projectChatRealTimeHome_roomsButtonAcess'>ACESSAR</div>
-                                
-                            </>
-                        ) : null}
-                    </div>
-            ))}
-            <br/>
-            <button onClick={handleShowNewChatRoom}>Nova Sala</button>
-            <br/>
-            {divShowInputChatRoom &&
-                <>
-                    <br/>
-                    <label><b className='projectChatRealTimeHome_textYellow'>C A D A S T R O (S A L A)</b></label>
-                    <br/>
-                    <div className='projectChatRealTimeHomeRowGap'>
-                        <MdDriveFileRenameOutline size={24} />
-                        <label>Digite o nome da sala:</label>
-                    </div>
-                    <input type='text' placeholder='Nome' value={inputNameChatRoom} onChange={(e) => setInputNameChatRoom(e.target.value)}></input>
+                                    <div className='projectChatRealTimeHome_roomsButtonAcess'>ACESSAR</div>
+                                    
+                                </>
+                            ) : null}
+                        </div>
+                ))}
+                <br/>
+                <button onClick={handleShowNewChatRoom}>Nova Sala</button>
+                <br/>
+                {divShowInputChatRoom &&
+                    <>
+                        <br/>
+                        <label><b className='projectChatRealTimeHome_textYellow'>C A D A S T R O (S A L A)</b></label>
+                        <br/>
+                        <div className='projectChatRealTimeHomeRowGap'>
+                            <MdDriveFileRenameOutline size={24} />
+                            <label>Digite o nome da sala:</label>
+                        </div>
+                        <input type='text' placeholder='Nome' value={inputNameChatRoom} onChange={(e) => setInputNameChatRoom(e.target.value)}></input>
 
-                    <div className='projectChatRealTimeHomeRowGap'>
-                        <MdDescription size={24} />
-                        <label>Digite a descrição da sala:</label>
-                    </div>
-                    <input type='text' placeholder='Descrição' value={inputDescriptionChatRoom} onChange={(e) => setInputDescriptionChatRoom(e.target.value)}></input>
+                        <div className='projectChatRealTimeHomeRowGap'>
+                            <MdDescription size={24} />
+                            <label>Digite a descrição da sala:</label>
+                        </div>
+                        <input type='text' placeholder='Descrição' value={inputDescriptionChatRoom} onChange={(e) => setInputDescriptionChatRoom(e.target.value)}></input>
 
-                    <div className='projectChatRealTimeHomeRowGap'>
-                        <MdOutlinePrivacyTip size={24} />
-                        <label>Selecione a privacidade da sala:</label>
-                    </div>
-                    <div>*salas privadas só são acessadas com liberação do criador da sala.</div>
-                    <select id="access-select" value={inputSelectedAccess} onChange={handleAccessChange}>
-                        <option value="">Selecione</option>
-                        <option value="public">Pública</option>
-                        <option value="private">Privada</option>
-                    </select>
-                    <button onClick={handleNewChatRoom}>Criar Sala</button>
-                    <br />
-                    <br />
-                </>
-            }
-            {divAlert && 
-                <>
-                    <WarningComponent InfoText={divTextAlert}/>
-                </>
-            }
+                        <div className='projectChatRealTimeHomeRowGap'>
+                            <MdOutlinePrivacyTip size={24} />
+                            <label>Selecione a privacidade da sala:</label>
+                        </div>
+                        <div>*salas privadas só são acessadas com liberação do criador da sala.</div>
+                        <select id="access-select" value={inputSelectedAccess} onChange={handleAccessChange}>
+                            <option value="">Selecione</option>
+                            <option value="public">Pública</option>
+                            <option value="private">Privada</option>
+                        </select>
+                        <button onClick={handleNewChatRoom}>Criar Sala</button>
+                        <br />
+                        <br />
+                    </>
+                }
+                {divAlert && 
+                    <>
+                        <WarningComponent InfoText={divTextAlert}/>
+                    </>
+                }
+            </>
+            <div className='projectChatRealTimeHomeSecondButton' onClick={handleLogout}>Logout</div>
         </div>
     )
 }
